8d38f5b15bd65ebe487ef2f0946ba7443949a3d5
OmidZJ <zamanijamshidi.omid@gmail.com>
2025-01-18 06:02:48 +0330

    new version

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 
 # Load token from environment variables
 load_dotenv()
-TOKEN = os.getenv('7434733526:AAGsorRnGaWWzicG-yiWYdpACVIIR4aJwdI')
+TOKEN = os.getenv('TELEGRAM_TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
 # Folder to store downloaded files
